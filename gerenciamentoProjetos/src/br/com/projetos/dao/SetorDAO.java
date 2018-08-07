2ddf02f42d46363f0940a4b087ac828b11b71415
Smythy Costa <smythy.costa@hotmail.com>
2018-08-06 23:32:48 -0300

    finalizando a classe

--- gerenciamentoProjetos/src/br/com/projetos/dao/SetorDAO.java
+++ gerenciamentoProjetos/src/br/com/projetos/dao/SetorDAO.java
@@ -1,5 +1,70 @@
 package br.com.projetos.dao;
 
-public class SetorDAO {
+import javax.persistence.EntityManager;
 
+import br.com.projetos.jpa.EntityManagerUtil;
+import br.com.projetos.models.Setor;
+import br.com.projetos.util.UtilErros;
+import br.com.projetos.util.UtilMensagens;
+
+public class SetorDAO {
+	
+	private EntityManager em;
+
+	public SetorDAO(){
+		em = EntityManagerUtil.getEntityManager();
+	}
+	
+	public boolean gravar(Setor obj){
+		try {
+			em.getTransaction().begin();
+			if (obj.getId() == null){
+				em.persist(obj);
+			} else {
+				em.merge(obj);
+			}
+			em.getTransaction().commit();
+			UtilMensagens.mensagemInformacao("Objeto persistido com sucesso!");
+			return true;
+		} catch (Exception e){
+			if (em.getTransaction().isActive() == false){
+				em.getTransaction().begin();
+			}
+			em.getTransaction().rollback();
+			UtilMensagens.mensagemErro("Erro ao persistir objeto: "+
+												UtilErros.getMensagemErro(e));
+			return false;
+		}
+	}
+	
+	public boolean excluir(Setor obj){
+		try {
+			em.getTransaction().begin();
+			em.remove(obj);
+			em.getTransaction().commit();
+			UtilMensagens.mensagemInformacao("Objeto removido com sucesso!");
+			return true;
+		} catch (Exception e){
+			if (em.getTransaction().isActive() == false){
+				em.getTransaction().begin();
+			}
+			em.getTransaction().rollback();
+			UtilMensagens.mensagemErro("Erro ao remover objeto: "+
+												UtilErros.getMensagemErro(e));
+			return false;
+		}
+	}
+	
+	public Setor localizar(Integer id){
+		return em.find(Setor.class, id);
+	}
+
+	public EntityManager getEm() {
+		return em;
+	}
+
+	public void setEm(EntityManager em) {
+		this.em = em;
+	}
+	
 }
