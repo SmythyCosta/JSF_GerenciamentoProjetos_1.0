2b6af38578a4bd8b9b290670f969fdef1fb74731
Smythy Costa <smythy.costa@hotmail.com>
2019-02-03 20:21:16 -0300

    ajustes em TesteDAOGenerico

--- src/br/com/projetos/test/TesteDaoGenerico.java
+++ src/br/com/projetos/test/TesteDaoGenerico.java
@@ -2,13 +2,13 @@ package br.com.projetos.test;
 
 import java.util.List;
 
-import br.com.projetos.dao.SetorDAO;
+import br.com.projetos.dao.DAOSetor;
 import br.com.projetos.models.Setor;
 
 public class TesteDaoGenerico {
 
 	public static void main(String[] args) {
-		SetorDAO dao = new SetorDAO();
+		DAOSetor<Setor> dao = new DAOSetor<Setor>();
 		List<Setor> lista = dao.listar();
 		for (Setor o : lista){
 			System.out.println("Codigo: "+o.getId()+" Nome: "+o.getNome());
