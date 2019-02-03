8f42e6b39ee3c260d8f2623bd38aa516bc605e50
Smythy Costa <smythy.costa@hotmail.com>
2019-02-03 15:53:46 -0300

    teste dao gunerico

---
+++ src/br/com/projetos/test/TesteDaoGenerico.java
@@ -0,0 +1,18 @@
+package br.com.projetos.test;
+
+import java.util.List;
+
+import br.com.projetos.dao.SetorDAO;
+import br.com.projetos.models.Setor;
+
+public class TesteDaoGenerico {
+
+	public static void main(String[] args) {
+		SetorDAO<Setor> dao = new SetorDAO<Setor>();
+		List<Setor> lista = dao.listar();
+		for (Setor o : lista){
+			System.out.println("Codigo: "+o.getId()+" Nome: "+o.getNome());
+		}
+	}
+
+}
